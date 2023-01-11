6478c22315f2ce9d0f84036dc5c16ed6ace6c54b
sergeydomanov <nomad-labs@yandex.ru>
2023-01-11 20:22:31 +0300

    new

---
+++ src/main/java/ru/hogwarts/school/WebApplication.java
@@ -0,0 +1,9 @@
+package ru.hogwarts.school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplication {
+    public static void main(String[] args) {SpringApplication.run(WebApplication.class, args);}
+}
