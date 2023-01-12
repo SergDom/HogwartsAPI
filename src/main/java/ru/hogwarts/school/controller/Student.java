346e676aaa64e60702c083aed80a65d95d8d66e9
sergeydomanov <nomad-labs@yandex.ru>
2023-01-12 19:26:47 +0300

    new

--- src/main/java/ru/hogwarts/school/controller/Student.java
+++ src/main/java/ru/hogwarts/school/controller/Student.java
@@ -1,6 +1,5 @@
 package ru.hogwarts.school.controller;
 
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
